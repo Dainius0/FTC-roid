5fe9b623642239f71b7e3c77927752290e1ab028
Vasaris1 <vasarisrink@gmail.com>
2024-10-24 09:36:44 +0300

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,3 +1,14 @@
-x = flaot(input("Įveskite sumą, kurią norėtumėte išleisti:")
+x = float(input("Įveskite sumą, kurią norėtumėte išleisti:")
 print("Galite įsigyti:")
+
+
+
+
+
+
+
+
+
+elif x<1
+          print(f'Saldainiai, Traškučiai arba Kramtomoji guma')
 
