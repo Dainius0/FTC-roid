b57bb4f976757ddef859c77cbb96f5a3de7487d7
Dainius0 <115561775+Dainius0@users.noreply.github.com>
2024-11-06 08:49:32 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,10 +1,40 @@
-x = float(input("Įveskite sumą, kurią norėtumėte išleisti:")
+x = float(input("Įveskite sumą, kurią norėtumėte išleisti:"))
 print("Galite įsigyti:")
 
 if x >= 1.5: 
-  print(f'Saldainiai, Traškučiai, Gėrimas, Kramtomoji guma.')
+  print("1. Saldainiai")
+  print("2. Kramtomoji guma")
+  print("3. Traškučiai") 
+  print("4. Gėrimas")
 
-
-elif x<1:
-  print(f'Saldainiai, Traškučiai arba Kramtomoji guma')
-
+elif x>=1:
+  print("1. Saldainiai")
+  print("2. Kramtomoji guma")
+  print("3. Traškučiai")
+elif x>=0.75:
+  print("1. Saldainiai")
+  print("2. Kramtomoji guma")
+elif x>=0.5: 
+  print("1. Saldainiai")
+else:
+  print(f'Jūsų įvestos sumos nepakanka jokiems daiktams įsigyti')
+        
+        
+a = int(input(f"Įveskite passirinkimo numerį: "))     
+if a == 1 and x >= 0.5:
+  print("Pasirinkote Saldainiai.")
+  b=x-0,5
+  print(f"ėkojame už Jūsų pirkinį. Jūsų likutis yra €{b}.")
+  
+if a == 2 and x >= 0.75:
+  print("Pasirinkote Kramtomoji guma.")
+  b=x-0,75
+  print(f"ėkojame už Jūsų pirkinį. Jūsų likutis yra €{b}.")
+  
+if a == 3 and x>=1:
+  print("Pasirinkote Traškučiai.")
+  b=x-1
+  print(f"ėkojame už Jūsų pirkinį. Jūsų likutis yra €{b}.")
+if a == 4 and x >= 1.5:
+  print("Pasirinkote Gėrimas.")
+  b=x-1,5
